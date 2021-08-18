ad7f79a8a8973b5907f7b17cd50d59e5881a4a15
Dumidu Sathsara <85824106+DumiduSathsara@users.noreply.github.com>
2021-08-18 13:58:29 +0530

    Update __main__.py

--- StreamMusic/__main__.py
+++ StreamMusic/__main__.py
@@ -1,5 +1,5 @@
 # StreamMusic (Telegram bot project)
-# Copyright (C) 2021  Sadew Jayasekara & Rojserbest
+# Copyright (C) 2021 Dumidu Sathsara , Sadew Jayasekara & Rojserbest
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
